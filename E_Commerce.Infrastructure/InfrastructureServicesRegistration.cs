8e28bbb0122d0de6fd07180976b1d3778ac60382
karim12kh <karim236@gmail.com>
2026-06-28 19:25:05 +0200

    Add project files.

---
+++ E_Commerce.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,16 @@
+﻿using E_Commerce.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace E_Commerce.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices( this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<StoreDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+    }
+}
